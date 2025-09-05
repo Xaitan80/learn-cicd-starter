ed5f5cdcdedaa38da7c6c54d50bce6237c42f4c3
Xaitan <utrustad.sprang_0z@icloud.com>
2025-09-05 12:46:03 +0200

    trying to send to gcp and deploy

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o notely
+# Build static linux/amd64 binary to match Dockerfile base image
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o notely .
